67043028b389b6b4241517fbac57cf67400948c6
Yanis Batura <yanis.batura@gmail.com>
2019-08-10 20:31:34 +0700

    Fix typos, improve grammar in Intrinsics.kt

--- libraries/stdlib/src/kotlin/coroutines/intrinsics/Intrinsics.kt
+++ libraries/stdlib/src/kotlin/coroutines/intrinsics/Intrinsics.kt
@@ -13,25 +13,25 @@ import kotlin.internal.InlineOnly
 
 /**
  * Obtains the current continuation instance inside suspend functions and either suspends
- * currently running coroutine or returns result immediately without suspension.
+ * the currently running coroutine or returns the result immediately without suspension.
  *
- * If the [block] returns the special [COROUTINE_SUSPENDED] value, it means that suspend function did suspend the execution and will
+ * If the [block] returns the special [COROUTINE_SUSPENDED] value, it means that the suspend function did suspend the execution and will
  * not return any result immediately. In this case, the [Continuation] provided to the [block] shall be
- * resumed by invoking [Continuation.resumeWith] at some moment in the
+ * resumed by invoking [Continuation.resumeWith] at some point in the
  * future when the result becomes available to resume the computation.
  *
- * Otherwise, the return value of the [block] must have a type assignable to [T] and represents the result of this suspend function.
+ * Otherwise, the return value of the [block] must be of a type assignable to [T] and represents the result of this suspend function.
  * It means that the execution was not suspended and the [Continuation] provided to the [block] shall not be invoked.
  * As the result type of the [block] is declared as `Any?` and cannot be correctly type-checked,
  * its proper return type remains on the conscience of the suspend function's author.
  *
- * Invocation of [Continuation.resumeWith] resumes coroutine directly in the invoker's thread without going through the
- * [ContinuationInterceptor] that might be present in the coroutine's [CoroutineContext].
+ * Invocation of [Continuation.resumeWith] resumes the coroutine directly in the invoker's thread without going through the
+ * [ContinuationInterceptor] that might be present in the coroutine's [context][CoroutineContext].
  * It is the invoker's responsibility to ensure that a proper invocation context is established.
  * [Continuation.intercepted] can be used to acquire the intercepted continuation.
  *
- * Note that it is not recommended to call either [Continuation.resume] nor [Continuation.resumeWithException] functions synchronously
- * in the same stackframe where suspension function is run. Use [suspendCoroutine] as a safer way to obtain current
+ * Note that it is not recommended to call either [Continuation.resume] or [Continuation.resumeWithException] functions synchronously
+ * in the same stackframe where the suspension function is run. Use [suspendCoroutine] as a safer way to obtain the current
  * continuation instance.
  */
 @SinceKotlin("1.3")
@@ -41,7 +41,7 @@ public suspend inline fun <T> suspendCoroutineUninterceptedOrReturn(crossinline 
     throw NotImplementedError("Implementation of suspendCoroutineUninterceptedOrReturn is intrinsic")
 
 /**
- * This value is used as a return value of [suspendCoroutineUninterceptedOrReturn] `block` argument to state that
+ * This value is used as the return value of [suspendCoroutineUninterceptedOrReturn]'s `block` argument to state that
  * the execution was suspended and will not return any result immediately.
  *
  * **Note: this value should not be used in general code.** Using it outside of the context of
